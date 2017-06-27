5a486bbe261ca14729678b234cfecb7e63bf6715
edward <edward@genomoncology.com>
2017-06-27 09:55:15 -0400

    WIP stuff

---
+++ src/rigor/reporting/cucumber.py
@@ -0,0 +1,30 @@
+import enum
+import os
+import aiohttp
+import jmespath
+import ast
+
+from mako.template import Template
+
+import related
+
+from rigor.model import Result, Case, Step
+from django.db import models
+
+class Feature(object):
+    uri: related.StringField(Cucumber)
+    keyword: models.CharField(required=True, default='Feature')
+    name: related.StringField(Cucumber.feature.name, required=True)
+
+
+
+
+
+class Cucumber(object):
+    elements = related.SequenceField()
+    steps = related.SequenceField(Step)
+    examples = related.ImmutableDict(Case.scenarios, required=False)
+    feature = related.ImmutableDict(Result.case)
+
+    def to_report(self, step, scenario, result):
+        elements =
