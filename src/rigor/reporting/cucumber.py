8817326f2e500f4bd9045cdbe778d80fdd7d3e84
edward <edward@genomoncology.com>
2017-06-30 13:06:57 -0400

    Implemented cucumber json responses for failing tests - WIP

--- src/rigor/reporting/cucumber.py
+++ src/rigor/reporting/cucumber.py
@@ -1,12 +1,6 @@
 import json
 import urllib
-from collections import deque
-
-import jmespath
 import related
-from django.db import models
-
-from rigor.model import Suite
 
 
 @related.immutable
@@ -37,24 +31,64 @@ class Match(object):
 
 
 @related.immutable
+class Validators(object):
+    actual = related.ChildField(object)
+    expect = related.ChildField(object)
+
+    @classmethod
+    def create_failing(cls, actual, expect):
+        return cls(
+            actual=actual,
+            expect=expect
+        )
+
+
+@related.immutable
+class DocString(object):
+    value = related.StringField()
+    content_type = related.StringField()
+    line = related.IntegerField()
+
+    @classmethod
+    def create_failure(cls, step, failure):
+        v = []
+        for fail_validation in failure.fail_validations:
+            i = 0
+            for item in fail_validation.actual:
+                if fail_validation.actual[i] != fail_validation.expect[i]:
+                    v.append(Validators.create_failing(fail_validation.actual[i], fail_validation.expect[i]))
+                i += 1
+        spacer = "====================\n"
+
+        return cls(
+            value=spacer + " FAILED VALIDATIONS\n" + spacer + related.to_json(json.loads(related.to_json(v))),
+            content_type="application/json",
+            line=5
+        )
+
+
+@related.immutable
 class Result(object):
     status = related.StringField()
+    line = related.IntegerField()
     #todo: figure out durations & error message
     duration = related.IntegerField()
-    # error_message = related.StringField(required=False, repr=False, cmp=False)
 
     @classmethod
     def create(cls, step):
+
         for validations in step.validate:
             if validations.actual != validations.expect:
                 status = "failed"
                 return cls(
                         status=status,
+                        line=4,
                         duration=1
                 )
             status = "passed"
             return cls(
                 status=status,
+                line=4,
                 duration=1
             )
 
@@ -63,53 +97,57 @@ class Result(object):
 class Step(object):
     keyword = related.StringField()
 
-    # line = related.IntegerField()
+    line = related.IntegerField()
     match = related.ChildField(Match)
-    name = related.StringField(default="Step")
+    name = related.StringField()
+    doc_string = related.ChildField(DocString)
     result = related.ChildField(Result, required=False, default=None)
 
     @classmethod
-    def create(cls, step):
+    def create(cls, step, failure):
+
         return cls(
             keyword="",
+            line=3,
             name=step.description,
+            doc_string=DocString.create_failure(step, failure),
             match=Match.create(step),
             result=Result.create(step)
         )
 
 
-@related.immutable
-class Row(object):
-    # line = related.IntegerField()
-    id = related.StringField()
-    cells = related.SequenceField(str)
-
-    @classmethod
-    def create_first(cls, example, case):
-        dct = example.result[0]
-        dct = dct.keys()
-        cells = []
-        for key in dct:
-            temp = key
-            cells.append(temp)
-        return cls(
-            cells=cells,
-            id=urllib.parse.quote_plus(case.name) + ";" + str(Element.name) + ";" + "PlaceHolder" + ";1"
-            # todo: Find a way tor replace "PlaceHolder" with example.name
-        )
-
-    @classmethod
-    def create(cls, example, case, i):
-        dct = example.result[0]
-        dct = dct.keys()
-        cells = []
-        for key in dct:
-            cells.append(example.result[i][key])
-        return cls(
-            cells=cells,
-            id=urllib.parse.quote_plus(case.name) + ";" + str(Element.name) + ";" + "PlaceHolder" + ";%s" % i
-            # todo: Find a way to replace "PlaceHolder" with example.name
-        )
+# @related.immutable
+# class Row(object):
+#     # line = related.IntegerField()
+#     id = related.StringField()
+#     cells = related.SequenceField(str)
+#
+#     @classmethod
+#     def create_first(cls, example, case):
+#         dct = example.result[0]
+#         dct = dct.keys()
+#         cells = []
+#         for key in dct:
+#             temp = key
+#             cells.append(temp)
+#         return cls(
+#             cells=cells,
+#             id=urllib.parse.quote_plus(case.name) + ";" + str(Element.name) + ";" + "PlaceHolder" + ";1"
+#             # todo: Find a way tor replace "PlaceHolder" with example.name
+#         )
+#
+#     @classmethod
+#     def create(cls, example, case, i):
+#         dct = example.result[0]
+#         dct = dct.keys()
+#         cells = []
+#         for key in dct:
+#             cells.append(example.result[i][key])
+#         return cls(
+#             cells=cells,
+#             id=urllib.parse.quote_plus(case.name) + ";" + str(Element.name) + ";" + "PlaceHolder" + ";%s" % i
+#             # todo: Find a way to replace "PlaceHolder" with example.name
+#         )
 
 
 # @related.immutable
@@ -149,19 +187,15 @@ class Element(object):
     description = related.StringField()
     type = related.StringField()
     steps = related.SequenceField(Step, default=[])
-    # examples = related.SequenceField(Example, required=False, default=None)
     tags = related.SequenceField(Tag, required=False, default=None)
 
 
     @classmethod
-    def create(cls, case):
+    def create(cls, case, failure):
         stp = []
-        exp = []
         tg = []
         for step in case.steps:
-            stp.append(Step.create(step))
-        # for scenario in case.scenarios:
-        #     exp.append(Example.create(case, scenario))
+            stp.append(Step.create(step, failure))
         for tag in case.tags:
             tg.append(Tag.create(tag))
         return cls(
@@ -169,11 +203,10 @@ class Element(object):
             # Scenario name
             name="EGFR L858R and Non-Small Cell Lung Carcinoma",
             id=(urllib.parse.quote_plus(case.name) + ";PlaceHolder"),
-            line=5,
+            line=2,
             description="",
             type="scenario",
             steps=stp,
-            # examples=exp,
             tags=tg
         )
 
@@ -184,25 +217,25 @@ class Feature(object):
     keyword = related.StringField()
     id = related.StringField()
     name = related.StringField()
-    # line = related.IntegerField()
+    line = related.IntegerField()
     elements = related.SequenceField(Element, default=[])
     description = related.StringField(required=False, default=None)
 
-    # tags = related.SequenceField(Tag, required=False, default=None)
-
     @classmethod
-    def create(cls, case):
+    def create(cls, case, failure):
         elm = []
         for scenario in case.scenarios:
-            elm.append(Element.create(case))
+            elm.append(Element.create(case, failure))
 
         return cls(
             uri=case.file_path,
             keyword="Feature",
             id=urllib.parse.quote_plus(case.name),
             name=case.name,
+            line=1,
             elements=elm,
         )
+
 
 @related.immutable
 class Cucumber(object):
@@ -211,14 +244,8 @@ class Cucumber(object):
     def load_init(self, suite):
         ret = []
         for failure in suite.failed:
-            ret.append(Feature.create(failure.case))
+            ret.append(Feature.create(failure.case, failure))
         return ret
-
-        # raw = Feature.create(suite.failed[0].case)
-        # print(str(raw))
-        # temp = related.to_json(raw)
-        # cuke = json.loads(temp)
-        # return cuke
 
 
 
