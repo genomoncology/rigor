f101a4eb05624fcd8f72b1e4b77597d2d53741b9
Ian Maurer <ian@genomoncology.com>
2017-07-01 08:28:07 -0400

    v0.0.7 - More CLI options.

--- src/rigor/cli.py
+++ src/rigor/cli.py
@@ -11,9 +11,27 @@ import sys
 @click.argument('directories', nargs=-1)
 @click.option('--domain', default="http://localhost:8000",
               help="Domain name (e.g. http://localhost:8000)")
-def main(directories, domain):
+@click.option('--include', '-i', multiple=True,
+              help="Include tag of cases. (e.g. smoke)")
+@click.option('--exclude', '-e', multiple=True,
+              help="Exclude tag of cases to run. (e.g. broken)")
+@click.option('--prefix', '-p', multiple=True,
+              help="Filter cases by file prefix. (e.g. smoke_)")
+@click.option('--extensions', '-e', multiple=True,
+              help="Filter cases by file extension. (e.g. rigor)")
+@click.option('--concurrency', '-c', type=int, default=20,
+              help='# of concurrent HTTP requests. (default: 20)')
+def main(directories, domain, include, exclude, prefix, extensions,
+         concurrency):
+    # remove preceding . if provided in extension (.rigor => rigor)
+    extensions = [ext[1:] if ext.startswith(".") else ext
+                  for ext in extensions or []]
+
     # collect
-    suite = Suite(directories=directories, domain=domain)
+    suite = Suite(directories=directories, domain=domain,
+                  tags_included=include, tags_excluded=exclude,
+                  file_prefixes=prefix, extensions=extensions,
+                  concurrency=concurrency)
 
     # execute
     suite.execute()
