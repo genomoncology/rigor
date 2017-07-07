31f5d8bf8a87d6c3aeac5e14852e9342aade9252
Ian Maurer <ian@genomoncology.com>
2017-07-07 13:46:02 -0400

    v0.0.8 - Fix in related UUID fields require v0.4 of related.

--- src/rigor/__init__.py
+++ src/rigor/__init__.py
@@ -41,7 +41,7 @@ __all__ = [
 
 __author__ = """Ian Maurer"""
 __email__ = 'ian@genomoncology.com'
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 
 __uri__ = "http://www.github.com/genomoncology/rigor"
 __copyright__ = "Copyright (c) 2017 genomoncology.com"
